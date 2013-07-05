bac8b243c2be61b83c1f63f3eaf28bc3f321b287
ekinoguz <ekinoguz@gmail.com>
2013-07-05 00:05:17 -0700

    adding test for equals and compareTo()

--- unittests/com/sprout/foopoker/gamelogic/HandTypeTest.java
+++ unittests/com/sprout/foopoker/gamelogic/HandTypeTest.java
@@ -28,7 +28,7 @@ public class HandTypeTest {
 	}
 
 	@Test
-	public void test_getCorrectType() {
+	public void test_GetCorrectType() {
 		HandType t1 = new HandType(HandType.FLUSH_ROYALE);
 		assertEquals(t1.getType(), HandType.FLUSH_ROYALE);
 		
@@ -40,7 +40,7 @@ public class HandTypeTest {
 	}
 	
 	@Test
-	public void test_getMessage() {
+	public void test_GetMessage() {
 		HandType t1 = new HandType(HandType.FLUSH_ROYALE);
 		assertNotNull(t1.getMessage());
 		
@@ -49,5 +49,38 @@ public class HandTypeTest {
 		
 		HandType t3 = new HandType(HandType.HIGH_CARD);
 		assertNotNull(t3.getMessage());
+	}
+	
+	@Test
+	public void test_Equals() {
+		HandType t1 = new HandType(HandType.FLUSH_ROYALE);
+		HandType t2 = new HandType(HandType.FLUSH);
+		HandType t3 = new HandType(HandType.HIGH_CARD);
+		HandType t4 = new HandType(HandType.FLUSH_ROYALE);
+		assertFalse(t1.equals(t2));
+		assertFalse(t2.equals(t1));
+		assertFalse(t1.equals(t3));
+		assertFalse(t2.equals(t3));
+		assertTrue(t1.equals(t1));
+		assertTrue(t1.equals(t4));
+		assertTrue(t4.equals(t4));
+		assertTrue(t2.equals(t2));
+	}
+	
+	@Test
+	public void test_Compare() {
+		HandType t1 = new HandType(HandType.FLUSH_ROYALE);
+		HandType t2 = new HandType(HandType.FLUSH);
+		HandType t3 = new HandType(HandType.HIGH_CARD);
+		HandType t4 = new HandType(HandType.FLUSH_ROYALE);
+		assertTrue(t1.compareTo(t2) > 0);
+		assertTrue(t2.compareTo(t1) < 0);
+		assertTrue(t1.compareTo(t3) > 0);
+		assertTrue(t2.compareTo(t3) > 0);
+		assertTrue(t3.compareTo(t2) < 0);
+		assertTrue(t1.compareTo(t1) == 0);
+		assertTrue(t1.compareTo(t4) == 0);
+		assertTrue(t4.compareTo(t4) == 0);
+		assertTrue(t2.compareTo(t2) == 0);
 	}
 }
