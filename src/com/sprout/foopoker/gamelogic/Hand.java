c8374c4d04398506146d58baf6e5c3df1de49bd3
ekinoguz <ekinoguz@gmail.com>
2013-07-13 10:48:29 -0700

    converting tabs to spaces in Hand

--- src/com/sprout/foopoker/gamelogic/Hand.java
+++ src/com/sprout/foopoker/gamelogic/Hand.java
@@ -11,82 +11,68 @@ import java.util.ArrayList;
 
 public class Hand {
 
-	private ArrayList<Card> hand;
-	
-	private HandType type;
-	private Card highCard;
-	
-	private final static int CARD_SIZE = 7;
-	
-	public Hand() {
-		this.hand = new ArrayList<Card>();
-	}
-	
-	/**
-	 * 
-	 * @param newCard the newCard which will be added to hand
-	 * @throws ArrayIndexOutOfBoundsException if you are trying to add
-	 * 	CARD_SIZE+1=8'th card.
-	 */
-	public void appendCard(Card newCard) throws ArrayIndexOutOfBoundsException{
-		if (hand.size() == CARD_SIZE) {
-			throw new ArrayIndexOutOfBoundsException();
-		}
-		this.hand.add(newCard);	
-	}
-	
-	/**
-	 * 
-	 * @param index the index of the card which we want
-	 * @return the Card with the given index
-	 * @throws ArrayIndexOutOfBoundsException if index < 0 or index >= CARD_SIZE
-	 */
-	public Card getCard(int index) throws ArrayIndexOutOfBoundsException{
-		if (index < 0 || index >= CARD_SIZE) {
-			throw new ArrayIndexOutOfBoundsException();
-		}
-		return hand.get(index);
-	}
-	
-	/**
-	 * @return hand which is ArrayList<Card>
-	 */
-	public ArrayList<Card> getCards() {
-		return this.hand;
-	}
-	
-	/**
-	 * @return the size of the hand
-	 */
-	public int getSize() {
-		return this.hand.size();
-	}
-	
-	/**
-	 * @return the type of the Hand
-	 */
-	public HandType getType() {
-		return type;
-	}
-	
-	/**
-	 * @return the high card of hand
-	 */
-	public Card getHighCard() {
-		return highCard;
-	}
-	
-	/**
-	 * @return String representation of the hand
-	 */
-	public String toString() {
-		return hand.toString();
-	}
-	
-	/**
-	 * @return the Android-friendly message according to the type
-	 */
-	public String getMessage() {
-		return type.getMessage() + " with " + highCard;
-	}
+  private ArrayList<Card> hand;
+  private static final int CARD_SIZE = 7;
+  
+  /** Initialize a new Hand with nothing */
+  public Hand() {
+    this.hand = new ArrayList<Card>();
+  }
+  
+  /**
+   * @param newCard the newCard which will be added to hand
+   * @throws ArrayIndexOutOfBoundsException if you are trying to add
+   *   8'th card.
+   */
+  public void appendCard(Card newCard) throws ArrayIndexOutOfBoundsException{
+    if (hand.size() == CARD_SIZE) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    this.hand.add(newCard);  
+  }
+  
+  /**
+   * @param newCards the newCards which will be added to the hand
+   * @throws ArrayIndexOutOfBoundsException if there will be more than 7 cards
+   *   after the addition
+   */
+  public void appendCards(ArrayList<Card> newCards) throws ArrayIndexOutOfBoundsException{
+    if (hand.size()+newCards.size() > CARD_SIZE) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    this.hand.addAll(newCards);  
+  }
+  
+  /**
+   * @param index the index of the card which we want
+   * @return the Card with the given index
+   * @throws ArrayIndexOutOfBoundsException if index < 0 or index >= 7
+   */
+  public Card getCard(int index) throws ArrayIndexOutOfBoundsException{
+    if (index < 0 || index >= CARD_SIZE) {
+      throw new ArrayIndexOutOfBoundsException();
+    }
+    return hand.get(index);
+  }
+  
+  /**
+   * @return the hand which is ArrayList<Card>
+   */
+  public ArrayList<Card> getCards() {
+    return this.hand;
+  }
+  
+  /**
+   * @return the size of the hand
+   */
+  public int getSize() {
+    return this.hand.size();
+  }
+  
+  /**
+   * @return String representation of the hand
+   */
+  public String toString() {
+    return hand.toString();
+  }
 }
--- unittests/com/sprout/foopoker/gamelogic/HandTest.java
+++ unittests/com/sprout/foopoker/gamelogic/HandTest.java
@@ -2,76 +2,68 @@ package com.sprout.foopoker.gamelogic;
 
 import static org.junit.Assert.*;
 
+import java.util.ArrayList;
+
 import org.junit.Test;
 
 public class HandTest {
 
-	@Test
-	public void test_NotNull() {
-		Hand hand = new Hand();
-		assertNotNull(hand);
-	}
-	
-	@Test
-	public void test_AddAndRetriveCorrectly() {
-		Hand hand = new Hand();
-		Card card = new Card(1);
-		hand.appendCard(card);
-		assertEquals(card, hand.getCard(0));
-	}
-	
-	@Test
-	public void test_NotAddMoreThanCardSize() {
-		Hand hand = new Hand();
-		for (int i = 0; i < 7; i++) {
-			Card c = new Card(i+1);
-			hand.appendCard(c);
-			assertEquals(hand.getCard(i), c);
-		}
-		try{
-			hand.appendCard(new Card(39));
-			assertTrue(false);
-		} catch(Exception e) {
-			assertTrue(true);
-		}
-	}
+  @Test
+  public void test_NotNull() {
+    Hand hand = new Hand();
+    assertNotNull(hand);
+  }
+  
+  @Test
+  public void test_AddAndRetriveCorrectly() {
+    Hand hand = new Hand();
+    Card card = new Card(1);
+    hand.appendCard(card);
+    assertEquals(card, hand.getCard(0));
+  }
+  
+  @Test
+  public void test_NotAddMoreThanCardSize() {
+    Hand hand = new Hand();
+    for (int i = 0; i < 7; i++) {
+      Card c = new Card(i+1);
+      hand.appendCard(c);
+      assertEquals(hand.getCard(i), c);
+    }
+    try{
+      hand.appendCard(new Card(39));
+      assertTrue(false);
+    } catch(Exception e) {
+      assertTrue(true);
+    }
+  }
 
-	@Test(expected=ArrayIndexOutOfBoundsException.class)
-	public void test_SizeCheck() {
-		Hand hand = new Hand();
-		for (int i = 0; i < 7; i++) {
-			hand.appendCard(new Card(i+1));
-			assertEquals(hand.getSize(), (i+1));
-		}
-		for (int i = 0; i < 100; i++) {
-			hand.appendCard(new Card(5));
-			assertEquals(hand.getSize(), 7);
-		}
-	}
-	
-	@Test
-	public void test_NullHighCard() {
-		Hand hand = new Hand();
-		assertNull(hand.getHighCard());
-		Card card = new Card(1);
-		hand.appendCard(card);
-		assertNull(hand.getHighCard());
-	}
-	
-	@Test
-	public void test_NotNullHighCard() {
-		
-	}
-	
-	public void test_EqualHand() {
-		
-	}
-	
-	public void test_BetterHand() {
-		
-	}
-	
-	public void test_WorseHand() {
-		
-	}
+  @Test(expected=ArrayIndexOutOfBoundsException.class)
+  public void test_SizeCheck() {
+    Hand hand = new Hand();
+    for (int i = 0; i < 7; i++) {
+      hand.appendCard(new Card(i+1));
+      assertEquals(hand.getSize(), (i+1));
+    }
+    for (int i = 0; i < 100; i++) {
+      hand.appendCard(new Card(5));
+      assertEquals(hand.getSize(), 7);
+    }
+  }
+  
+  @Test(expected=ArrayIndexOutOfBoundsException.class)
+  public void test_AppendCards() {
+    Hand hand = new Hand();
+    ArrayList<Card> cards = new ArrayList<Card>();
+    for (int i = 1; i <= 7; i++) {
+      cards.add(new Card(i));
+    }
+    hand.appendCards(cards);
+    assertEquals(hand.getSize(), 7);
+    for (int i = 1; i <= 7; i++) {
+      assertEquals(hand.getCard(i-1), new Card(i));
+    }
+    hand.appendCard(new Card(22));
+    hand.appendCards(cards);
+  }
 }
