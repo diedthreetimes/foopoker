8f8839ead0b8de90594f36e64bb8431cc6f5a88d
sky faber <diedthreetimes@gmail.com>
2013-10-19 10:27:11 -0600

    ActivityTest requires to test a specific activity.

--- FooPokerTest/src/com/sprout/foopoker/test/userdata/UserTest.java
+++ FooPokerTest/src/com/sprout/foopoker/test/userdata/UserTest.java
@@ -3,24 +3,25 @@ package com.sprout.foopoker.test.userdata;
 import com.sprout.foopoker.userdata.User;
 import com.sprout.foopoker.userdata.UserDatabaseHandler;
 
-import android.test.ActivityTestCase;
+import android.test.AndroidTestCase;
 import android.test.RenamingDelegatingContext;
 
-public class UserTest extends ActivityTestCase{
+// TODO: Should probably use ProviderTestCase2<ContentProvider>
+//     This requires using a ContentProvider (which abstracts storage location)
+public class UserTest extends AndroidTestCase{
 
 	private UserDatabaseHandler handler;
 	
 	@Override
 	public void setUp() throws Exception {
-	  super.setUp();
+		super.setUp();
 	  
 		RenamingDelegatingContext context = new RenamingDelegatingContext(
-				getActivity().getApplicationContext(), "test_");
+				getContext(), "test_");
 		handler = new UserDatabaseHandler(context);
 	}
 	
 	public void test_IsUserNull() {
-		System.out.println("ASD");
 		User user = new User();
 		assertNotNull(user);
 	}
